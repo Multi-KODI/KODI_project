b5517349a5d4a8f17a9048257f542d564c3e2cc0
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2024-01-03 00:47:58 +0900

    [ADD] Project initial structure

---
+++ KODI/src/main/java/dto/PostDTO.java
@@ -0,0 +1,19 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDTO {
+	private int postIdx;
+	private String title;
+	private String content;
+	private String regdate;
+	private String address;
+	private int memberIdx;
+}

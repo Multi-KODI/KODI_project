094b843c9a893e6ed6f0b43ff58943dab7f3dcc7
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2024-01-10 23:22:01 +0900

    [UPDATE] 채팅 리스트 페이지 백엔드 구현

--- KODI/src/main/java/dto/ChatListFriendDTO.java
+++ KODI/src/main/java/dto/ChatListFriendDTO.java
@@ -10,6 +10,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatListFriendDTO {
-	private Integer friendMemberIdx;
+	private int friendMemberIdx;
 	private String friendMemberName;
 }

5248f799a9c2f0c43881cf3ac815e147e7cf5bb8
HIHI <97479821+YGIEN@users.noreply.github.com>
2024-01-31 10:57:33 +0900

    [DELETE] 코드 제거

--- KODI/src/main/java/dao/MyPageDAO.java
+++ KODI/src/main/java/dao/MyPageDAO.java
@@ -17,24 +17,10 @@ public interface MyPageDAO {
 	
 	//나의 전체글 가져오기 SQL문
 	List<PostDTO> readMyPosts(int memberIdx);
-	//서로이웃
-	List<FriendDTO> allFriends(int memberIdx);
-	//내가 추가한 사용자
-	List<FriendDTO> mySideFriends(int memberIdx);
-	//나를 추가한 사용자
-	List<FriendDTO> otherSideFriends(int memberIdx);
 	//친구 정보 조회
 	List<MemberDTO> friendInfo(List<Integer> friendList);
 	//전체국가조회
   List<FlagDTO> allFlags();
 	//전체 이미지 조회
 	List<PostImageDTO> allImages();
-	//서로 이웃 삭제
-	void pairDelete(Integer member_Idx, Integer memberIdx);
-	//팔로잉 삭제
-	void followingDelete(Integer member_Idx, Integer memberIdx);
-	//팔로워 수락
-  void followerAccept(Integer member_Idx, Integer memberIdx);
-	//팔로워 삭제
-	void followerDelete(Integer member_Idx, Integer memberIdx);
 }
--- KODI/src/main/java/service/MyPageService.java
+++ KODI/src/main/java/service/MyPageService.java
@@ -21,22 +21,7 @@ public class MyPageService {
     List<PostDTO> myPosts = myPageDAO.readMyPosts(memberIdx);
     return myPosts;
   }
-  //서로이웃
-  public List<FriendDTO> allFriends(int memberIdx){
-    List<FriendDTO> allFriends = myPageDAO.allFriends(memberIdx);
-    return allFriends;
-  }
-
-  //내가 추가한 사용자
-  public List<FriendDTO> mySideFriends(int memberIdx){
-    List<FriendDTO> mySideFriends = myPageDAO.mySideFriends(memberIdx);
-    return mySideFriends;
-  }
-  //나를 추가한 사용자
-  public List<FriendDTO> otherSideFriends(int memberIdx){
-    List<FriendDTO> otherSideFriends = myPageDAO.otherSideFriends(memberIdx);
-    return otherSideFriends;
-  }
+ 
   //친구정보 조회
   public List<MemberDTO> friendInfo (List<Integer> friendList){
     List<MemberDTO> friendInfo = myPageDAO.friendInfo(friendList);
@@ -51,23 +36,4 @@ public class MyPageService {
     return myPageDAO.allImages();
   }
 
-  //서로 이웃 삭제
-  public void pairDelete(Integer member_Idx, Integer memberIdx){
-    myPageDAO.pairDelete(member_Idx, memberIdx);
-  }
-  
-  //팔로잉 삭제
-  public void followingDelete(Integer member_Idx, Integer memberIdx) {
-    myPageDAO.followingDelete(member_Idx, memberIdx);
-  }
-
-  //팔로워 수락
-  public void followerAccept(Integer member_Idx, Integer memberIdx) {
-    myPageDAO.followerAccept(member_Idx, memberIdx);
-  }
-
-  //팔로워 삭제
-  public void followerDelete(Integer member_Idx, Integer memberIdx) {
-    myPageDAO.followerDelete(member_Idx, memberIdx);
-  }
 }

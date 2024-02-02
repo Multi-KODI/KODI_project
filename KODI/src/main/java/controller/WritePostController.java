1b5ff333f5d22debbe469ea878cd04bc53e12784
PGH322 <basillio322@gamil.com>
2024-02-02 11:24:33 +0900

    [UPDATE]게시글 작성 통합- Backend, jsp 수정

--- KODI/src/main/java/controller/WritePostController.java
+++ KODI/src/main/java/controller/WritePostController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,10 +10,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -39,17 +37,33 @@ public class WritePostController {
 	
 	//게시물 작성 페이지
 	@PostMapping("/post/issave")
-	public String isWrite(
-			@RequestParam("writePostDTO") WritePostDTO writePostDTO, 
-			@RequestParam("file") MultipartFile file, 
-			HttpSession session) {
+	public void isWrite(WritePostDTO writePostDTO, HttpSession session) 
+			throws IllegalStateException, IOException {
 		//세션 받아서 int 타입으로 변환
 		String sessionIdx = (String)session.getAttribute("memberIdx");
 		Integer myMemberIdx = Integer.parseInt(sessionIdx);
+		System.out.println(myMemberIdx);
 		
-		List<File> imageFile = new ArrayList<File>();
-		
-		return writeservice.insertPost(writePostDTO, myMemberIdx);
+		//이미지 파일 이름 저장
+		List<String> fileName = new ArrayList<String>();
+		//받아온 파일들 저장
+		MultipartFile file[] = writePostDTO.getImagePost();
+		//이미지 파일들 로컬에 저장
+		String fileDir = "C:/FullStack/파이널 프로젝트/git/KODI_project/KODI/src/main/resources/static/image/db/";
+		String imagePath = "";
+		for(MultipartFile data : file) {
+			imagePath = fileDir + data.getOriginalFilename();
+			fileName.add(data.getOriginalFilename());
+			data.transferTo(new File(imagePath));
+		}
+		System.out.println(writePostDTO.getTitle());
+		System.out.println(writePostDTO.getContent());
+		System.out.println(writePostDTO.getCategory());
+		System.out.println(writePostDTO.getGrade());
+		System.out.println(writePostDTO.getAddress());
+		System.out.println(writePostDTO.getPostTags());
+		System.out.println(fileName.toString());
+		writeservice.insertPost(writePostDTO, fileName, myMemberIdx);
 		
 //		return "게시물이 성공적으로 작성되었습니다.";
 	}
--- KODI/src/main/java/service/WritePostService.java
+++ KODI/src/main/java/service/WritePostService.java
@@ -17,17 +17,15 @@ public class WritePostService {
 	@Qualifier("writepostdao")
 	WritePostDAO dao;
 	
-	public String insertPost(WritePostDTO writePostDTO, int myMemberIdx) {
+	public String insertPost(WritePostDTO writePostDTO, List<String> postImages, int myMemberIdx) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		
 		String title = writePostDTO.getTitle();
 		String content = writePostDTO.getContent();
-		String regdate = writePostDTO.getRegdate();
 		String address = writePostDTO.getAddress();
 		String category = writePostDTO.getCategory();
 		double grade = writePostDTO.getGrade();
 		List<String> postTags = writePostDTO.getPostTags();
-		List<String> postImages = writePostDTO.getPostImages();
 		
 		//필수 작성요소 확인(제목, 내용, 주소)
 		if(title.isBlank()) {
@@ -44,7 +42,6 @@ public class WritePostService {
 		map.put("myMemberIdx", myMemberIdx);
 		map.put("title", title);
 		map.put("content", content);
-		map.put("regdate", regdate);
 		map.put("address", address);
 		map.put("category", category);
 		map.put("grade", grade);
@@ -54,16 +51,20 @@ public class WritePostService {
 		int postIdx = dao.selectPostIdx(myMemberIdx);
 
 		//게시글 태그 저장
-		map.clear();
-		map.put("postIdx", postIdx);
-		map.put("postTags", postTags);
-		dao.insertPostTags(map);
+		for(int i=0; i<postTags.size(); i++) {
+			map.clear();
+			map.put("postIdx", postIdx);
+			map.put("postTags", postTags.get(i));
+			dao.insertPostTags(map);
+		}
 		
 		//게시글 이미지 저장
-		map.clear();
-		map.put("postIdx", postIdx);
-		map.put("postImages", postImages);
-		dao.insertPostImages(map);
+		for(int i=0; i<postImages.size(); i++) {
+			map.clear();
+			map.put("postIdx", postIdx);
+			map.put("postImages", postImages.get(i));
+			dao.insertPostImages(map);
+		}
 		
 		return "작성을 완료하였습니다.";
 	}

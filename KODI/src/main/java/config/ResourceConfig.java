bf475c6079bde90b1dd33cc2a5aaed8c7e100d74
PGH322 <basillio322@gamil.com>
2024-02-16 02:18:35 +0900

    [update]경로 오류 수정

--- KODI/src/main/java/config/ResourceConfig.java
+++ KODI/src/main/java/config/ResourceConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ResourceConfig implements WebMvcConfigurer {
 
 	private String connectPath = "/image/**";
-	private String resourcePath = "file:./image/db/";
+	private String resourcePath = "file:/usr/mydir/image";
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
